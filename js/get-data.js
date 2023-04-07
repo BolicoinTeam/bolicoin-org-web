37a022852dc3b27f6f07b4b24a78f3387af371ba
Johan Rojas <97570183+redsnahoj@users.noreply.github.com>
2023-04-07 07:14:35 -0400

    refactoring script

--- js/get-data.js
+++ js/get-data.js
@@ -1,33 +1,36 @@
-// Price
-const httpPrice = new XMLHttpRequest();
-httpPrice.onload = function() {
-  const data = this.responseText;
-  document.getElementById("price").innerHTML = Number(data).toFixed(4);
-  document.getElementById('usd').classList.remove('d-none');
-  document.getElementById('price').classList.add('indicator-data');
+function fetchData(url, callback) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => callback(data))
+    .catch((error) => console.error('Error fetching data:', error));
 }
-httpPrice.open("GET", "https://chainz.cryptoid.info/boli/api.dws?q=ticker.usd");
-httpPrice.send();
+
+function updateElement(elementId, value, classToAdd, classToRemove) {
+  const element = document.getElementById(elementId);
+  element.innerHTML = value;
+  element.classList.add(classToAdd);
+
+  if (classToRemove) {
+    element.classList.remove(classToRemove);
+  }
+}
+
+// Price
+fetchData('https://chainz.cryptoid.info/boli/api.dws?q=ticker.usd', (data) => {
+  const price = Number(data).toFixed(4);
+  updateElement('price', price, 'indicator-data', 'd-none');
+  document.getElementById('usd').classList.remove('d-none');
+});
 
 // Hashrate
-
-const httpHashrate = new XMLHttpRequest();
-httpHashrate.onload = function() {
-  const data = this.responseText;
-  document.getElementById("hashrate").innerHTML = Number(data).toFixed(2);
+fetchData('https://chainz.cryptoid.info/boli/api.dws?q=hashrate', (data) => {
+  const hashrate = Number(data).toFixed(2);
+  updateElement('hashrate', hashrate, 'indicator-data', 'd-none');
   document.getElementById('ghs').classList.remove('d-none');
-  document.getElementById('hashrate').classList.add('indicator-data');
-}
-httpHashrate.open("GET", "https://chainz.cryptoid.info/boli/api.dws?q=hashrate");
-httpHashrate.send();
+});
 
 // Master Nodes
-
-const httpMasterNodes = new XMLHttpRequest();
-httpMasterNodes.onload = function() {
-  const data = JSON.parse(this.responseText);
-  document.getElementById("masterNodes").innerHTML = data.masterNodeCount;
-  document.getElementById('masterNodes').classList.add('indicator-data');
-}
-httpMasterNodes.open("GET", "https://chainz.cryptoid.info/boli/api.dws?q=masternodeinfo");
-httpMasterNodes.send();
+fetchData('https://chainz.cryptoid.info/boli/api.dws?q=masternodeinfo', (data) => {
+  const parsedData = JSON.parse(data);
+  updateElement('masterNodes', parsedData.masterNodeCount, 'indicator-data');
+});
